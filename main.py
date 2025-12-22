6d1dd4ecac0e3473d9bcd04f6e88899884b6813e
vinicius <viniciusnicacio80@gmail.com>
2025-12-22 07:12:56 -0300

    arquivos locais

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def get_book_text(content: str):
+    return content
+    
+
+def main():
+    get_book_text(file_content)
+
+with open("/books/frankenstein.txt") as f:
+    file_content = f.read()
